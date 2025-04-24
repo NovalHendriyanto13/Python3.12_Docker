ec39a3acebfd49682ea7a12177c7554712f10e2e
Noval <NovalHendriyanto@idsmed.com>
2025-04-24 13:56:46 +0700

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,3 @@
-cd pyhton_docker
-git pull origin lambda
 docker build -t python_docker .
 docker tag python_docker:latest 381014012748.dkr.ecr.us-west-1.amazonaws.com/python_docker:latest
 aws ecr get-login-password --region us-west-1 | docker login --username AWS --password-stdin 381014012748.dkr.ecr.us-west-1.amazonaws.com
