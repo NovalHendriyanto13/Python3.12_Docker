cc88fad5577adc988d732bffd7bec68c4bb573b1
Noval <noshi@LAPTOP-2IBVL950.localdomain>
2026-06-26 15:21:14 +0700

    update

--- configs/constants.py
+++ configs/constants.py
@@ -10,3 +10,84 @@ campaign_status = {
     "Active": 0,
     "Archived": 1
 }
+
+action_type_code = {
+    3: "IMPRESSION",
+    4: "REDEMPTION",
+    5: "CLICK_THROUGH",
+    6: "IN_STORE_REDEMPTION",
+    7: "APP_STARTUP",
+    8: "REGISTRATION",
+    9: "LOGIN",
+    12: "IMPRESSION",
+    13: "CLLICK",
+    14: "DOWNLOAD",
+    20: "IMPRESSION",
+    26: "POINTS_CREATION",
+    27: "CARD_ACTIVATION",
+    28: "REWARD_ACTIVATION"
+
+}
+
+advertisement_status = {
+    0: "Draft",
+    1: "Published",
+    2: "Deleted"
+}
+
+registration_type = {
+    0: "Device Account",
+    1: "Email Registered"
+}
+
+consumer_type = {
+    0: "Email Registered",
+    1: "Non Digital (Call Center)",
+    2: "Device Registration"
+}
+
+loyalty_program_status = {
+    0: "Draft",
+    1: "Published",
+    2: "Deleted"
+}
+
+loyalty_program_type = {
+    0: "Stamp Program",
+    1: "Point Program",
+}
+
+message_status = {
+    0: "Unpublished",
+    1: "Publish",
+    2: "Approving",
+    3: "Approve",
+    4: "Decline",
+    5: "Declined",
+    6: "Published",
+    7: "Archive",
+    8: "Archived",
+    9: "Delete",
+    10: "Deleted",
+    11: "CancelApproval"
+}
+
+channel_type = {
+    1:"Push Message",
+    2:"Email",
+    3:"SMS"
+}
+
+redemption_code_types = {
+    0: "Single Image",
+    1: "Single Text Code",
+    2: "Auto Generated",
+    3: "Predefined List",
+    4: "Short Code"
+}
+
+offer_status = {
+    0: "Draft",
+    1: "Published",
+    2: "Deleted"
+}
